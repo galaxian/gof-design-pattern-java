d64fdba419284881ca430513e8619cb3d8335ac8
Lee <94841127+galaxian@users.noreply.github.com>
2023-04-04 17:23:23 +0900

    test(prototype) : tree 리스트 생성 후 리스트 복제 테스트

--- src/test/java/prototype/TreePrototypeTest.java
+++ src/test/java/prototype/TreePrototypeTest.java
@@ -3,6 +3,10 @@ package prototype;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TreePrototypeTest {
@@ -39,5 +43,28 @@ public class TreePrototypeTest {
 
         assertThat(position).isEqualTo(pineTree.getPosition());
         assertThat(otherPosition).isEqualTo(otherPineTree.getPosition());
+    }
+
+    @Test
+    @DisplayName("tree 리스트 생성 후 리스트 복제 테스트")
+    void tree_리스트_생성_후_리스트_복제_테스트() {
+        double mass = 10.0;
+        double height = 3.7;
+        Position position = new Position(3, 7);
+        Position otherPosition = new Position(4, 8);
+
+        PlasticTree plasticTree = new PlasticTree(mass, height);
+        plasticTree.setPosition(position);
+        PineTree pineTree = new PineTree(mass, height);
+        pineTree.setPosition(otherPosition);
+
+        List<Tree> trees = Arrays.asList(plasticTree, pineTree);
+        List<Tree> treeClones = trees.stream().map(Tree::copy).collect(Collectors.toList());
+
+        Tree plasticTreeClone = treeClones.get(0);
+
+        assertThat(mass).isEqualTo(plasticTreeClone.getMass());
+        assertThat(height).isEqualTo(plasticTree.getHeight());
+        assertThat(position).isEqualTo(plasticTree.getPosition());
     }
 }
