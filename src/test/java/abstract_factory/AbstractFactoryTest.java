841c25a14c1e1cec9d609ff419a8d2dd8997fc99
Lee <94841127+galaxian@users.noreply.github.com>
2023-04-10 22:56:40 +0900

    test(abstract factory) : MotorVehicle의 motor, wheel type 테스트

--- src/test/java/abstract_factory/AbstractFactoryTest.java
+++ src/test/java/abstract_factory/AbstractFactoryTest.java
@@ -7,15 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AbstractFactoryTest {
     @Test()
-    @DisplayName("d")
-    void test1() {
+    @DisplayName("MotorVehicle의 motor, wheel type 테스트")
+    void MotorVehicle의_motor_wheel_type_테스트() {
         AbstractFactory abstractFactory;
 
         abstractFactory = FactoryProvider.getFactory("motor");
         Motor motor = abstractFactory.createMotor();
         Wheel wheel = abstractFactory.createWheel();
 
-        assertThat(motor.type()).isEqualTo("Motor");
         assertThat(motor).isInstanceOf(MotorVehicleMotor.class);
+        assertThat(wheel).isInstanceOf(MotorVehicleWheel.class);
     }
 }
