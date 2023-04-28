b5f4c19cb3af6d31eef6afc276ba2a23c8347b42
Lee <94841127+galaxian@users.noreply.github.com>
2023-04-28 21:23:13 +0900

    feat(decorator) : 문자열의 시작과 끝에 정해진 문자를 추가하는 Decorator 구현 클래스

---
+++ src/main/java/decorator/SideDecorator.java
@@ -0,0 +1,30 @@
+package decorator;
+
+public class SideDecorator extends Decorator{
+    private final char sideChar;
+
+    public SideDecorator(Item item, char sideChar) {
+        super(item);
+        this.sideChar = sideChar;
+    }
+
+    @Override
+    public int getRowsCount() {
+        return item.getRowsCount();
+    }
+
+    @Override
+    public int getColumnsCount() {
+        return item.getColumnsCount() + 2;
+    }
+
+    @Override
+    public int maxLength() {
+        return getRowsCount() * getColumnsCount();
+    }
+
+    @Override
+    public String getRowText(int row) {
+        return sideChar + item.getRowText(row) + sideChar;
+    }
+}
