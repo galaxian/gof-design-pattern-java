08836370fa0c13fd8792f072691fd852b2f06163
Lee <94841127+galaxian@users.noreply.github.com>
2023-05-22 20:21:01 +0900

    feat(bridge) : 나머지를 계산해주는 RemainderCalculate 작성

---
+++ src/main/java/bridge/RemainderCalculate.java
@@ -0,0 +1,23 @@
+package bridge;
+
+public class RemainderCalculate implements Calculate{
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public int div(int a, int b) {
+        return a % b;
+    }
+}
