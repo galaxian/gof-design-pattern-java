be1073450809143aca4a62b0e3b686ff68424ad3
Lee <94841127+galaxian@users.noreply.github.com>
2023-05-22 19:58:52 +0900

    feat(bridge) : 기능 클래스로 Calculate를 bridge로 사용해 구현부를 사용하도록 작성

---
+++ src/main/java/bridge/Calculator.java
@@ -0,0 +1,34 @@
+package bridge;
+
+public class Calculator {
+    protected Calculate calculate;
+
+    public Calculator(Calculate calculate) {
+        this.calculate = calculate;
+    }
+
+    public int add(int a, int b) {
+        return calculate.add(a, b);
+    }
+
+    public int sub(int a, int b) {
+        return calculate.sub(a, b);
+    }
+
+    public int mul(int a, int b) {
+        return calculate.mul(a, b);
+    }
+
+    public double div(int a, int b) {
+        return calculate.div(a, b);
+    }
+
+    public void printAllCalculate(int a, int b) {
+        System.out.println("-----------------");
+        System.out.println(a + " + " + b + " = " + add(a, b));
+        System.out.println(a + " - " + b + " = " + sub(a, b));
+        System.out.println(a + " * " + b + " = " + mul(a, b));
+        System.out.println(a + " / " + b + " = " + div(a, b));
+        System.out.println("-----------------");
+    }
+}
