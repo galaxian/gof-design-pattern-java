1aa3da2b2936b8e5cef16c9ec066ff1ece986c53
Lee <94841127+galaxian@users.noreply.github.com>
2023-05-22 20:30:12 +0900

    test(bridge) : 4칙 연산 및 몫 계산 테스트코드

---
+++ src/test/java/bridge/bridgeTest.java
@@ -0,0 +1,25 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class bridgeTest {
+
+    @Test
+    @DisplayName("4칙연산 테스트 및 div 메서드 몫 계산 테스트")
+    void 몫_계산테스트() {
+
+        int a = 72;
+        int b = 39;
+
+        Calculator quotientCalculator = new Calculator(new QuotientCalculate());
+
+        assertThat(quotientCalculator.add(a, b)).isEqualTo(a + b);
+        assertThat(quotientCalculator.sub(a, b)).isEqualTo(a - b);
+        assertThat(quotientCalculator.mul(a, b)).isEqualTo(a * b);
+
+        assertThat(quotientCalculator.div(a, b)).isEqualTo(a / b);
+    }
+}
