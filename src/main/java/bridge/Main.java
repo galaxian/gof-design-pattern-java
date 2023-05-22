fb713d16345b446d02e1b13d51e219cfc4353dc8
Lee <94841127+galaxian@users.noreply.github.com>
2023-05-22 20:22:39 +0900

    refactor(bridge) : calculator -> quotientCalculator 변수명 수정

--- src/main/java/bridge/Main.java
+++ src/main/java/bridge/Main.java
@@ -2,8 +2,8 @@ package bridge;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(new QuotientCalculate());
-        calculator.printAllCalculate(3, 4);
+        Calculator quotientCalculator = new Calculator(new QuotientCalculate());
+        quotientCalculator.printAllCalculate(3, 4);
 
         Calculator remainderCalculator = new Calculator(new RemainderCalculate());
         remainderCalculator.printAllCalculate(3,4);
