48385c1b7fe5a85f0975429e859a93c6827718ee
Lee <94841127+galaxian@users.noreply.github.com>
2023-05-22 19:55:17 +0900

    feat(bridge) : 4칙 연산을 수행하는 클래스로 Calculate 인터페이스를 구현하는 클래스 작성

---
+++ src/main/java/bridge/CalculateImp.java
@@ -0,0 +1,23 @@
+package bridge;
+
+public class CalculateImp implements Calculate{
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public double div(int a, int b) {
+        return (double) a / (double)b;
+    }
+}
