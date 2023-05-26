76b7ba26fc4ee4e20b6264ff93b677bd349f3120
Lee <94841127+galaxian@users.noreply.github.com>
2023-05-26 18:22:33 +0900

    feat(facade) : main 메서드 작성

---
+++ src/main/java/facade/Main.java
@@ -0,0 +1,29 @@
+package facade;
+
+public class Main {
+    public static void main(String[] args) {
+        DBMS dbms = new DBMS();
+        Cache cache = new Cache();
+
+        String name = "Robert";
+
+        Row row = cache.get(name);
+
+        if (row == null) {
+            row = dbms.query(name);
+            if (row != null) {
+                cache.put(row);
+            }
+        }
+
+        if (row != null) {
+            Message message = new Message(row);
+
+            System.out.println(message.makeName());
+            System.out.println(message.makeBirthday());
+            System.out.println(message.makeEmail());
+        } else {
+            System.out.println(name + " is not exists");
+        }
+    }
+}
