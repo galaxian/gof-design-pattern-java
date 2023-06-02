b60ee5ac29fd5245c941e0b11d6e041d673ffb7b
Lee <94841127+galaxian@users.noreply.github.com>
2023-06-02 22:37:08 +0900

    feat(flyweight) : intrinsic 정보를 담당하는 Controller 클래스 작성

---
+++ src/main/java/flyweight/Controller.java
@@ -0,0 +1,11 @@
+package flyweight;
+
+public class Controller {
+    void on() {
+        System.out.println("제품을 동작합니다.");
+    }
+
+    void off() {
+        System.out.println("제품을 종료합니다.");
+    }
+}
